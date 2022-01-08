0f10dffddbb0ba463c9307dd3622894ae7270185
Jakub Kozłowski <kubukoz@gmail.com>
2022-01-08 02:11:10 +0100

    Split out Resolver

--- server/src/main/scala/steve/ServerSideExecutor.scala
+++ server/src/main/scala/steve/ServerSideExecutor.scala
@@ -2,31 +2,24 @@ package steve
 
 import cats.implicits.*
 import cats.MonadThrow
+import cats.ApplicativeThrow
 
 object ServerSideExecutor {
 
-  def instance[F[_]: Interpreter: MonadThrow]: Executor[F] =
+  def instance[F[_]: Interpreter: Resolver: MonadThrow]: Executor[F] =
     new Executor[F] {
       private val emptyHash: Hash = Hash(Vector.empty)
       private val emptySystem: SystemState = SystemState(Map.empty)
 
-      private val resolveCommand: Build.Command => ResolvedBuild.Command = {
-        case Build.Command.Upsert(k, v) => ResolvedBuild.Command.Upsert(k, v)
-        case Build.Command.Delete(k)    => ResolvedBuild.Command.Delete(k)
-      }
-
-      private def resolve(build: Build): F[ResolvedBuild] = (build == Build.empty)
-        .guard[Option]
-        .as(emptySystem)
-        .liftTo[F](new Throwable("Unsupported build!"))
-        .map { sys =>
-          ResolvedBuild(sys, build.commands.map(resolveCommand))
+      def build(
+        build: Build
+      ): F[Hash] = Resolver[F]
+        .resolve(build)
+        .flatMap(Interpreter[F].interpret)
+        .flatMap {
+          case `emptySystem` => emptyHash.pure[F]
+          case _             => new Throwable("Unsupported system").raiseError[F, Hash]
         }
-
-      def build(build: Build): F[Hash] = resolve(build).flatMap(Interpreter[F].interpret).flatMap {
-        case `emptySystem` => emptyHash.pure[F]
-        case _             => new Throwable("Unsupported system").raiseError[F, Hash]
-      }
 
       def run(hash: Hash): F[SystemState] = (hash == emptyHash)
         .guard[Option]
@@ -37,7 +30,37 @@ object ServerSideExecutor {
 
   def module[F[_]: MonadThrow]: Executor[F] = {
     given Interpreter[F] = Interpreter.instance[F]
+    given Resolver[F] = Resolver.instance[F]
     instance[F]
   }
+
+}
+
+trait Resolver[F[_]] {
+  def resolve(build: Build): F[ResolvedBuild]
+}
+
+object Resolver {
+  def apply[F[_]](using ev: Resolver[F]): Resolver[F] = ev
+
+  def instance[F[_]: ApplicativeThrow]: Resolver[F] =
+    new Resolver[F] {
+      private val emptyHash: Hash = Hash(Vector.empty)
+      private val emptySystem: SystemState = SystemState(Map.empty)
+
+      private val resolveCommand: Build.Command => ResolvedBuild.Command = {
+        case Build.Command.Upsert(k, v) => ResolvedBuild.Command.Upsert(k, v)
+        case Build.Command.Delete(k)    => ResolvedBuild.Command.Delete(k)
+      }
+
+      def resolve(build: Build): F[ResolvedBuild] = (build == Build.empty)
+        .guard[Option]
+        .as(emptySystem)
+        .liftTo[F](new Throwable("Unsupported build!"))
+        .map { sys =>
+          ResolvedBuild(sys, build.commands.map(resolveCommand))
+        }
+
+    }
 
 }
